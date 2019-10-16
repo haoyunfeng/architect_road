e023e534f50e0bd114162479e6c88819cc612e9c
jdd_haoyunfeng3 <haoyunfeng2@jd.com>
2019-10-16 12:09:36 +0800

    链表反转

--- java-basics/src/main/java/cn/haoyunfeng/datastruc/linkedlist/ReverseLinkedList.java
+++ java-basics/src/main/java/cn/haoyunfeng/datastruc/linkedlist/ReverseLinkedList.java
@@ -1,9 +1,33 @@
 package cn.haoyunfeng.datastruc.linkedlist;
 
 /**
- * @Description:
- * @author: haoyunfeng2
+ * @Description: 链表翻转
+ * @author: haoyunfeng
  * @Date: 2019/10/16 9:41 上午
  */
 public class ReverseLinkedList {
+
+
+    public static SingleLinkedList reverseList(SingleLinkedList singleLinkedList) {
+        SingleLinkedList.ListNode head = singleLinkedList.getHead();
+        SingleLinkedList.ListNode pre = null;
+        SingleLinkedList.ListNode current =head;
+        SingleLinkedList.ListNode next ;
+        while (current !=null){
+            singleLinkedList.setHead(current);
+            next = current.getNext();
+            current.setNext(pre);
+            pre = current;
+            current = next;
+        }
+        return singleLinkedList;
+    }
+    public static void main(String []args){
+        SingleLinkedList list = new SingleLinkedList();
+        for(int i=1;i<6;i++){
+            list.add(i);
+        }
+        System.out.println(list);
+        reverseList(list);
+    }
 }
--- java-basics/src/main/java/cn/haoyunfeng/datastruc/linkedlist/SingleLinkedList.java
+++ java-basics/src/main/java/cn/haoyunfeng/datastruc/linkedlist/SingleLinkedList.java
@@ -1,9 +1,43 @@
 package cn.haoyunfeng.datastruc.linkedlist;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
- * @Description:
- * @author: haoyunfeng2
+ * @Description: 单链表
+ * @author: haoyunfeng
  * @Date: 2019/10/16 10:08 上午
  */
-public class SingleLinkedList {
+public class SingleLinkedList{
+    @Getter
+    @Setter
+    private ListNode head;
+    private ListNode current;
+    @Getter
+    private int size;
+    public void addFirst(int val){
+        head = new ListNode(val);
+        current = head;
+        size ++;
+    }
+    public void add(int val){
+        if (head == null){
+            addFirst(val);
+        }else {
+            ListNode node = new ListNode(val);
+            current.next = node;
+            current = node;
+            size++;
+        }
+    }
+
+    public class ListNode {
+        int val;
+
+        @Getter
+        @Setter
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
 }
