4b6e11c842024c533b00da2331feaca21177edfa
jdd_haoyunfeng3 <haoyunfeng2@jd.com>
2019-10-28 21:20:18 +0800

    排序练习

--- java-basics/src/main/java/cn/haoyunfeng/datastruc/practice/sort/SelectionSort.java
+++ java-basics/src/main/java/cn/haoyunfeng/datastruc/practice/sort/SelectionSort.java
@@ -1,17 +1,31 @@
 package cn.haoyunfeng.datastruc.practice.sort;
 
 /**
- * @Description:
+ * @Description: 选择排序，和冒泡排序类似。比冒泡少了交换次数
  * @author: haoyunfeng2
  * @Date: 2019/10/28 7:01 下午
  */
 public class SelectionSort {
 
     public static void selectionSort(int []array){
+        for(int i=0;i<array.length;i++){
+            int minindex = i;
+            for(int j = i+1;j<array.length;j++){
+                if(array[minindex] > array[j]){
+                    minindex = j;
+                }
+            }
 
+            int temp = array[i];
+            array[i] = array[minindex];
+            array[minindex] = temp;
+
+        }
+        System.out.println(array);
     }
 
     public static void main(String[]args){
-
+        int[]array = {4,5,6,1,2,3};
+        selectionSort(array);
     }
 }
